330d457dfffdc8f2bdbee79973a58d12db7678b3
Olivier Huin <olih@users.noreply.github.com>
2025-01-07 09:25:57 +0000

    Adds block kind

--- src/incy-wincy-model.ts
+++ src/incy-wincy-model.ts
@@ -1,5 +1,5 @@
 import {describeEnum} from 'faora-kai';
-import {z} from 'zod';
+import { z } from 'zod';
 
 const programmingLanguageKey = ['js', 'ts', 'dart', 'python'] as const;
 
@@ -13,3 +13,25 @@ const programmingLanguageLabels = {
 const programmingLanguage = z
   .enum(programmingLanguageKey)
   .describe(describeEnum('Encryption strength:', programmingLanguageLabels));
+
+const blockKindKey = [
+  'class',
+  'interface',
+  'const',
+  'function',
+  'test',
+  'other',
+] as const;
+
+const blockKindlabels = {
+  class: 'Class',
+  interface: 'Interface',
+  const: 'Constant',
+  function: 'Function',
+  test: 'Test',
+  other: 'Other',
+};
+
+const blockKind = z
+  .enum(blockKindKey)
+  .describe(describeEnum('Encryption strength:', blockKindlabels));
