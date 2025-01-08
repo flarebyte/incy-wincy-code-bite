0c6a3a50ddceacf359ad3f5518fbeffab67f5b99
Olivier Huin <olih@users.noreply.github.com>
2025-01-08 09:29:15 +0000

    Adds class section

--- src/incy-wincy-model.ts
+++ src/incy-wincy-model.ts
@@ -1,4 +1,4 @@
-import {describeEnum} from 'faora-kai';
+import { describeEnum, stringEffectFields, stringFields } from 'faora-kai';
 import { z } from 'zod';
 
 const programmingLanguageKey = ['js', 'ts', 'dart', 'python'] as const;
@@ -37,3 +37,19 @@ const blockKindlabels = {
 const blockKind = z
   .enum(blockKindKey)
   .describe(describeEnum('Language block kind:', blockKindlabels));
+
+const codeSection = {
+  name: stringFields.string1To80.describe('Name of the class'),
+  body: stringFields.string1To1000.describe('The full code of the block'),
+  uncommentedBody: stringFields.string1To1000.describe(
+    'The code of the block with comments removed'
+  ),
+  keywords: stringFields.string1To1000
+    .optional()
+    .describe('Sorted keywords from the code'),
+};
+
+const classSection = z.object({
+  kind: z.literal('class').describe('Class'),
+  ...codeSection,
+});
