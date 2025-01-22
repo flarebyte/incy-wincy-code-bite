bb16ba0642ad48fbc4fc08abd49a2bccab68ad5d
Olivier Huin <olih@users.noreply.github.com>
2025-01-22 10:10:42 +0000

    wip

--- src/incy-parser.ts
+++ src/incy-parser.ts
@@ -1,5 +1,9 @@
-import {type IncyWincySourceModel} from './incy-wincy-model.js';
+import { type IncyWincySourceModel } from './incy-wincy-model.js';
 
 export type IncyWincyCodeParser = {
   parse(sourceFilename: string, code: string): IncyWincySourceModel;
+};
+
+export type IncyWincySectionDetector = {
+  startClass(line: string): boolean;
 };
