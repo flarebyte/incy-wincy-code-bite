4c0fada39cdb60ef13ab3f7e71c2ed0fe70c3565
Olivier Huin <olih@users.noreply.github.com>
2025-01-30 09:28:29 +0000

    Extract child info

--- src/incy-js-parser.ts
+++ src/incy-js-parser.ts
@@ -1,6 +1,6 @@
 import Parser from 'tree-sitter';
-import {type IncyWincyCodeParser} from './incy-parser.js';
-import {type IncyWincySourceModel} from './incy-wincy-model.js';
+import { type IncyWincyCodeParser } from './incy-parser.js';
+import { type IncyWincySourceModel } from './incy-wincy-model.js';
 import javaScript from 'tree-sitter-javascript';
 
 const parser = new Parser();
@@ -9,7 +9,35 @@ parser.setLanguage(javaScript);
 export class IncyWincyJavascriptParser implements IncyWincyCodeParser {
   parse(sourceFilename: string, code: string): IncyWincySourceModel {
     const tree = parser.parse(code);
-    console.log(tree);
+    const { children } = tree.rootNode;
+    for (const child of children) {
+      const {
+        type,
+        text,
+        hasError,
+        isExtra,
+        isMissing,
+        startIndex,
+        startPosition,
+        endIndex,
+        endPosition,
+        descendantCount,
+      } = child;
+      const childInfo = {
+        type,
+        text,
+        hasError,
+        isExtra,
+        isMissing,
+        startIndex,
+        startPosition,
+        endIndex,
+        endPosition,
+        descendantCount,
+      };
+      console.log(childInfo);
+    }
+
     const sourceModel: IncyWincySourceModel = {
       programmingLanguage: 'js',
       sourceFilename,
