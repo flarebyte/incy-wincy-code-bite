313ed5ceaffacee6bfe08331d1af345acdc03578
Olivier Huin <olih@users.noreply.github.com>
2025-01-30 18:59:14 +0000

    Adds name

--- src/incy-js-parser.ts
+++ src/incy-js-parser.ts
@@ -6,37 +6,43 @@ import javaScript from 'tree-sitter-javascript';
 const parser = new Parser();
 parser.setLanguage(javaScript);
 
+const asChildInfo = (child: Parser.SyntaxNode) => {
+  const {
+    type,
+    text,
+    hasError,
+    isExtra,
+    isMissing,
+    startIndex,
+    startPosition,
+    endIndex,
+    endPosition,
+    descendantCount,
+  } = child;
+  const nameNode = child.childForFieldName('name');
+  const name = nameNode ? nameNode.text : undefined;
+  const childInfo = {
+    type,
+    text,
+    hasError,
+    isExtra,
+    isMissing,
+    startIndex,
+    startPosition,
+    endIndex,
+    endPosition,
+    descendantCount,
+    name,
+  };
+  return childInfo;
+};
+
 export class IncyWincyJavascriptParser implements IncyWincyCodeParser {
   parse(sourceFilename: string, code: string): IncyWincySourceModel {
     const tree = parser.parse(code);
     const { children } = tree.rootNode;
-    for (const child of children) {
-      const {
-        type,
-        text,
-        hasError,
-        isExtra,
-        isMissing,
-        startIndex,
-        startPosition,
-        endIndex,
-        endPosition,
-        descendantCount,
-      } = child;
-      const childInfo = {
-        type,
-        text,
-        hasError,
-        isExtra,
-        isMissing,
-        startIndex,
-        startPosition,
-        endIndex,
-        endPosition,
-        descendantCount,
-      };
-      console.log(childInfo);
-    }
+    const childrenInfo = children.map(asChildInfo);
+    console.log(JSON.stringify(childrenInfo));
 
     const sourceModel: IncyWincySourceModel = {
       programmingLanguage: 'js',
